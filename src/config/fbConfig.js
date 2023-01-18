36bf59ecfbbb55eea1894f4b57bee2d603395135
unknown <aniakoruc@gmail.com>
2023-01-18 09:25:09 +0100

    hide firebase's keys in env

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -1,20 +1,20 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "@firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAuAdA2zXoYZPVa36gIqa_fAWgmf2Wdp74",
-  authDomain: "shortly-frontendmaster.firebaseapp.com",
-  projectId: "shortly-frontendmaster",
-  storageBucket: "shortly-frontendmaster.appspot.com",
-  messagingSenderId: "894751491751",
-  appId: "1:894751491751:web:04b2dc9b2d1e4a9e0dbd82",
-  measurementId: "G-CTMT6MWD4V",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const appFirebase = initializeApp(firebaseConfig);
-const analytics = getAnalytics(appFirebase);
+// const analytics = getAnalytics(appFirebase);
 export const auth = getAuth();
 export const db = getFirestore(appFirebase);
 
